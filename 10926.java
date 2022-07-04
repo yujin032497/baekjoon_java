8868513a8d835c100f27048ac93cac70c3b2b217
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-05 06:21:51 +0900

    Create 10926.java

---
+++ 10926.java
@@ -0,0 +1,20 @@
+// 2022-07-05
+// ??!
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		String id;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		id = br.readLine();
+		
+		System.out.println(id+"??!");
+	}
+
+}
