dbecf7f173622f947d740a0f1ccede63080a7617
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-08 23:51:43 +0900

    Create 10171.java

---
+++ 10171.java
@@ -0,0 +1,16 @@
+// 2022-07-08
+// 고양이
+
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
