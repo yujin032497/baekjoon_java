d9f5968263c86b688c993ae26964eca438921cdc
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-11 13:46:10 +0900

    Create 10699.java

---
+++ 10699.java
@@ -0,0 +1,13 @@
+// 2022-07-11
+// 오늘 날짜
+
+import java.time.LocalDate;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+	}
+}
