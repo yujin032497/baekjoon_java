f72f12f55f8c47013576fe0fa44980ccc3d7a1a0
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-13 23:28:07 +0900

    Create 2439.java

---
+++ 2439.java
@@ -0,0 +1,31 @@
+// 2022-07-13
+// 별 찍기 - 2
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		
+		int N;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+		}
+	}
+
+}
