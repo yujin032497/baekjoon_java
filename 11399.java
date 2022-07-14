037f3895863e5c77a624d54f1f6fdaca7bbdb88f
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-14 23:11:08 +0900

    Update 11399.java

--- 11399.java
+++ 11399.java
@@ -9,28 +9,28 @@ import java.util.Arrays;
 
 public class Main {
 	
-	static int N;
-	static int [] time;
-	static int prev, sum;
+	static int N; //사람 수
+	static int [] time; //사람별 인출하는데 걸리는 시간
+	static int prev, sum; //누적 시간, 총시간
 	
 	public static void main(String args[]) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine()); //사람 수 입력
 		
 		time = new int [N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i=0; i<N; i++) {
-			time[i] = Integer.parseInt(st.nextToken());
+			time[i] = Integer.parseInt(st.nextToken()); //사람별 인출하는데 걸리는 시간 입력
 		}
 		
 		Arrays.sort(time);
 		
 		for(int i=0; i<N; i++) {
-			sum += prev + time[i];
-			prev += time[i];
+			sum += prev + time[i]; //총시간
+			prev += time[i]; //
 		}
 		
 		System.out.println(sum);
