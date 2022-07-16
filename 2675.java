419885aa473faf914fc492210f16b3ada89d2faf
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-16 23:32:07 +0900

    Update 2675.java

--- 2675.java
+++ 2675.java
@@ -20,20 +20,21 @@ public class Main {
 		
 		T = Integer.parseInt(br.readLine());
 		
-    //문자열 입력
+    		//문자열 입력
 		for(int i=0; i<T; i++) {
 			st = new StringTokenizer(br.readLine());
 			R = Integer.parseInt(st.nextToken());
 			S = st.nextToken();
 			P = "";
-      //문자 반복을 통한 문자열 생성
+			
+     			 //문자 반복을 통한 문자열 생성
 			for(int j=0; j<S.length(); j++) {
 				for(int k=0; k<R; k++) {
 					P += S.charAt(j);
 				}
 			}
 			
-      //출력
+      			//출력
 			System.out.println(P);
 		}
 	}
