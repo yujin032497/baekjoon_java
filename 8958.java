3c39326a2e94791047a859211651cd9c86a8445b
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-25 19:32:03 +0900

    Create 8958.java

---
+++ 8958.java
@@ -0,0 +1,32 @@
+// 2022-07-25
+//OX퀴즈
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); //테스트 케이스 수
+		for(int i=0; i<N; i++) {
+			int point=0, sum=0; //연속 O의 점수, 점수 합계
+			String str = br.readLine();
+			for(int j=0; j<str.length(); j++) {
+				char c = str.charAt(j); //한 문자씩 읽기
+				if(c=='O') { //O이면
+					point++; //점수 1씩 증가
+				} else {
+					point = 0; //점수 0
+				}
+				sum+=point; //+1증가된 점수를 계속 더하기
+			}
+			
+			System.out.println(sum); //
+		}
+		
+	}
+
+}
