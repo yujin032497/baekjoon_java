5b1db2a6f91a0da01492e279f2ae0005e05167c9
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-07-27 23:45:00 +0900

    Update 1049.java

--- 1049.java
+++ 1049.java
@@ -1,6 +1,3 @@
-// 2022-07-27
-// 기타줄
-
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,7 +13,8 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken()); //끊어진 기타줄
 		int M = Integer.parseInt(st.nextToken()); //브랜드 개수
 		
-        	//세트 가격, 낱개 가격 초기화는 1000 (가격은 1000보다 작거나 같을 수 있으므로)
+       		//세트 가격, 낱개 가격 초기화는 1000 (가격은 1000보다 작거나 같을 수 있으므로)
+        	//최종가격도 1000보다 작거나 같다는 문제해석을 잘못하지 말 것...!!!! 
 		int min_unit_price = 1000, min_set_price = 1000;
 		int price = Integer.MAX_VALUE; //최종 최소 가격
 		
@@ -26,16 +24,16 @@ public class Main {
 			min_unit_price = Math.min(min_unit_price,Integer.parseInt(st.nextToken())); //기타 낱개 가격 중 최소값
 		}
 		/*각각의 최솟값의 경우를 min()으로 비교하여 최솟값 구하기*/
+
+        //세트: N/6, 낱개: N%6
+        //세트로만 구입했을 경우
+        price = Math.min(price, (N/6+1) * min_set_price);
+
 		//낱개로만 구입했을 경우
 		price = Math.min(price, min_unit_price*N);
 		
         //낱개와 세트를 함께 구입했을 경우
 		price = Math.min(price, ((N/6) * min_set_price) + ((N%6) * min_unit_price));
-		
-        //세트로만 구입했을 경우
-        price = Math.min(price, (N/6+1) * min_set_price);
-		
 		System.out.println(price);
 	}
-
 }
