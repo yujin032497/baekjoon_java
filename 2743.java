495a89e88670db302197ae7f0cab7ba6e38835ae
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2022-09-17 04:34:33 +0900

    Create 2743.java

---
+++ 2743.java
@@ -0,0 +1,16 @@
+// 2022-09-17
+// 단어 길이 재기
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String str = in.readLine();
+		
+		System.out.println(str.length());
+	}
+}
