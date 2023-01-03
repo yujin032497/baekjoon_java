8e233f7f409005a89752876c80daa0fde94a2def
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2023-01-04 05:32:09 +0900

    Create 8393.java

---
+++ 8393.java
@@ -0,0 +1,21 @@
+// 2022-01-04
+// 합
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        int sum = 0;
+
+        for(int i=1; i<=n; i++) {
+            sum+=i;
+        }
+
+        System.out.println(sum);
+    }
+}
