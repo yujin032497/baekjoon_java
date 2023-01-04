a2255370f509a864bbd758e9e62191adcc596c9e
yujin032497 <49347652+yujin032497@users.noreply.github.com>
2023-01-05 05:03:36 +0900

    Create 1271.java

---
+++ 1271.java
@@ -0,0 +1,17 @@
+// 2022-01-05
+// 엄청난 부자2
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger(); // 입력 범위 때문에 BigInteger로 받는다.
+        BigInteger m = in.nextBigInteger();
+        in.close();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+    }
+}
